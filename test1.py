ee337f06d4305cce06b987cce5a3269e76a685a3
talib-sid <talib.siddiqui@students.iiit.ac.in>
2025-03-05 11:12:07 +0530

    changed stuff in original files

--- test1.py
+++ test1.py
@@ -4,3 +4,6 @@ import random
 
 x = random.uniform(1, 10)
 print(x)
+
+
+print(x + random.binomialvariate(1, 0.5))
--- test2.py
+++ test2.py
@@ -1,2 +1,6 @@
 # this is test 2
 
+import torch
+
+nn = torch.nn.Linear(1, 1)
+print(nn)
